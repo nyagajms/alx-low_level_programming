8830209260b716aa3bb91425410215c33b4c326e
James <122010077+nyagajms@users.noreply.github.com>
2023-08-14 20:10:38 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - This function allocate memory using malloc.
- * @b: unsigned input integer
- * Return: A pointer to allocated memory or NULL if it fails
- */
+* malloc_checked - This function allocate memory using malloc.
+* @b: unsigned input integer
+* Return: A pointer to allocated memory or NULL if it fails
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *s;
 	s = malloc(b);
+	
 	if (s == NULL)
 		exit(98);
 	return (s);
