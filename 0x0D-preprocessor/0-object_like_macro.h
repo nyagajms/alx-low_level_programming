f0e14f1c4b97495c7300fac22c717d8c29322838
James <122010077+nyagajms@users.noreply.github.com>
2023-08-14 22:38:29 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO_H_
-#define _OBJECT_LIKE_MACRO_H_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
 #define SIZE 1024
-#endif /* _OBJECT_LIKE_MACRO_H_ */
+
+#endif
