711ea9cac264b53d4e7b6d7f5aacad982ee6d8c5
James <122010077+nyagajms@users.noreply.github.com>
2023-08-24 17:49:51 -0400

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nelem;
 
-	i = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		nelem++;
 	}
 	return (i);
 }
