b8091e98186a948bcf01396ddb531c208b05f65b
James <122010077+nyagajms@users.noreply.github.com>
2023-08-24 17:53:07 -0400

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
